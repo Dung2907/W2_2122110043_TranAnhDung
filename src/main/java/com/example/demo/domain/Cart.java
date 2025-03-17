92a382ca5fede82e72db6a77a028a381e57af21a
Dung2907 <anhdungtran2015@gmail.com>
2025-03-17 11:11:29 +0700

    database

---
+++ src/main/java/com/example/demo/domain/Cart.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "carts")
+@JsonIgnoreProperties("customers")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;   
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+
+    // @OneToMany(mappedBy = "cart")
+    // private Set<CartItem> cartItems;
+    @JsonManagedReference
+    @OneToMany(mappedBy = "cart", targetEntity = CartItem.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<CartItem> cartItems = new HashSet<CartItem>();
+
+}
